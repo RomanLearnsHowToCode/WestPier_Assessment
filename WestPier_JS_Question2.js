a1c8a2c61fd2698d7ed37816fa44b707354c5ad7
RomanLearnsHowToCode <47738655+RomanLearnsHowToCode@users.noreply.github.com>
2021-11-14 19:59:58 +0000

    Add files via upload
    Answer  to Question 2 and reasoning

---
+++ WestPier_JS_Question2.js
@@ -0,0 +1,40 @@
+// WestPier studios - JavaScript:
+
+/* QUESTION 2: Original code */
+
+const myFunction = function() {
+     var myVar = 0;
+
+    return function() {
+         myVar++;
+        return myVar;
+    };
+};
+    
+const first = myFunction();
+const second = myFunction();
+
+console.log(first());
+console.log(first());
+console.log(second());
+console.log(second());
+console.log(first());
+
+/* ANSWER: 
+    This code would produce 1,2,1,2,3 
+    the reason for this is, that const first and const second are new instances of myFunction, where myVar is being incremented every time it is returned,
+    we can prove this by creating a new function with text variable on line 28, line 30 declare var text which contains string text, 
+    we are logging to console on line 40 FUNCTION third, this will run the return function, assign new string to text variable and return it. */
+
+const textVariable = function () {
+
+    var text = "text";
+    
+    return function () {
+        text = "returnedText";
+        return text;
+    };
+}
+
+const third = textVariable();
+console.log(third());
