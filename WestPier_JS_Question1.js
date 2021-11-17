d7c5ddd342617d4fd003e40574899c92082cb827
RomanLearnsHowToCode <47738655+RomanLearnsHowToCode@users.noreply.github.com>
2021-11-17 15:18:59 +0000

    Update WestPier_JS_Question1.js
    Major improvement and upgrade..

--- WestPier_JS_Question1.js
+++ WestPier_JS_Question1.js
@@ -1,8 +1,11 @@
 // WestPier studios - JavaScript:
+console.log("**** WestPier Studios JavaScript ****");
+console.log(" ");
+console.log("*** QUESTION 1 PART 1: ***");
 
-/* QUESTION 1:  ORIGINAL CODE:
-
-const obj = {foo: ‘bar’};
+//QUESTION 1:  ORIGINAL CODE:
+console.log("Original code response:");
+const obj = {foo: 'bar'};
 
 obj.method = function() {
 
@@ -13,7 +16,8 @@ obj.method = function() {
 };
 
 obj.method();
-*/
+
+console.log(" ");
 
 /* QUESTION 1 PART 1:
 
@@ -23,12 +27,13 @@ obj.method();
 
 /* ANSWER PART 1:
 This will produce UNDEFINED,
-var logFunction on line 10 is global function
+var logFunction on line 12 is global function
 
-We can prove this by editing the line 10 by: console.log("My foo is:", this);
-this will produce the window object and global
+We can prove this by editing the line 12 by: console.log("My foo is:", this);
+this will produce the window object and global properties
 */
 
+console.log("*** QUESTION 1 PART 2: ***");
 
 /* QUESTION 1 PART 2:
 
@@ -37,53 +42,53 @@ this will produce the window object and global
 
 */
 
-
 /*1)We can modify the obj.method function and include the console.log inside, then we can delete the logFunction*/
+console.log("** 1st way resposne **");
 
-// const obj = {foo: 'bar'};
+const obj2_1 = {foo: 'bar'};
 
-// obj.method = function() {
-//     console.log("My foo is:", this.foo);
-// };
+obj2_1.method = function() {
+    console.log("My foo is:", this.foo);
+};
 
-// obj.method();
-
+obj2_1.method();
 
 /*2) We can include the function inside the obj object*/
+console.log("** 2nd way response **");
 
+const obj2_2 = {foo: 'bar',
+ logFunction(){
+    console.log("My foo is:", this.foo);
+}};
 
-// const obj = {foo: 'bar',
-//  logFunction(){
-//     console.log("My foo is:", this.foo);
-// }};
-
-// console.log(obj);
-
+console.log(obj2_2);
 
 /*3) Referencing obj.foo instead of this.foo*/
+console.log("** 3rd way response **");
+const obj2_3 = {foo: 'bar'};
 
-// const obj = {foo: 'bar'};
+obj2_3.method = function() {
+    var logFunction = function() { 
+        console.log("My foo is:", obj2_3.foo);
+    };
+    logFunction();
+};
 
-// obj.method = function() {
-//     var logFunction = function() { 
-//         console.log("My foo is:", obj.foo);
-//     };
-//     logFunction();
-// };
-
-// obj.method();
-
+obj2_3.method();
 
 /*4) We can parse obj into obj.method, and assigning this foo to obj.foo*/
+console.log("** 4th way response **");
+const obj2_4 = {foo: 'bar'};
 
-// const obj = {foo: 'bar'};
+obj2_4.method = function() {
+    var logFunction = function() {
+        this.foo = obj2_4.foo;
+        console.log("My foo is:", this.foo);
+    };
+    logFunction();
+};
 
-// obj.method = function() {
-//     var logFunction = function() {
-//         this.foo = obj.foo;
-//         console.log("My foo is:", this.foo);
-//     };
-//     logFunction();
-// };
+obj2_4.method(obj2_4);
 
-// obj.method(obj);
+console.log("*** END OF QUESTION 1 ***");
+console.log(" ");
